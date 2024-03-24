7d1daf4e96b992b9314e12a93dc799764ba37384
Pranav <hipranavs@outlook.com>
2024-03-23 21:03:44 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+
+def convert(char):
+    if char == " ":
+        return "/"
+    elif char not in MORSE_CODE_DICT:
+        return char
+    else:
+        return MORSE_CODE_DICT[char]
+
+
+text = input("What would you like to convert? ").upper()
+morse_code = ""
+for i in text:
+    new_char = convert(i)
+    morse_code += f"{new_char} "
+
+print(f"Morse code output: {morse_code}")
